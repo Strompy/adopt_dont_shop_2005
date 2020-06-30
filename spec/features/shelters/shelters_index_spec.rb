9e36592bd651c4817ab6019acd6f93ea3fb4a314
Phillip Strom <phillip.strom@gmail.com>
2020-06-30 17:19:08 -0600

    Create user story 2 test

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see all shelters names" do
+    shelter_1 = Shelter.new(name: "Hope")
+    shelter_2 = Shelter.new(name: "Paw Pals")
+
+    visit '/shelters'
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+  end
+end
