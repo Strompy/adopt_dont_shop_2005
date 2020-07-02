1b42cd6f3229b3bb09d7c621dc6704cf6ba15f1b
Phillip Strom <phillip.strom@gmail.com>
2020-07-01 19:37:04 -0600

    Create MVC for User Story 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get '/shelters', to: 'shelters#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/shelters/:id', to: 'shelters#show'
 end
---
+++ spec/models/shelter_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Shelter do
+  describe 'validations' do
+    it { should validate_presence_of :name}
+  end
+  describe 'methods' do
+
+  end
+end
